bcdf59940607eb339e7a2fbf5b0e4d23e08f2e4d
OfekHod <1xofekx1@gmail.com>
2020-07-02 19:55:51 +0300

    added download_sources.sh

---
+++ download_sources.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download Spark 2.4.6
+wget https://archive.apache.org/dist/spark/spark-2.4.6/spark-2.4.6-bin-hadoop2.7.tgz
+
+# Download Livy 0.7.0
+wget http://mirror.23media.de/apache/incubator/livy/0.7.0-incubating/apache-livy-0.7.0-incubating-bin.zip
