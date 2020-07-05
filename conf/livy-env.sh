f3a4916d3bc93faebce3c33024931bce8602e1de
OfekHod <1xofekx1@gmail.com>
2020-07-05 10:33:20 +0300

    remove SPARK_HOME from -e

---
+++ conf/livy-env.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+export SPARK_HOME = /opt/spark-2.4.6-bin-hadoop2.7
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@
 # Create directories
 mkdir $LIVY_HOME/logs
 
+# Create `livy-env.sh` file
+echo '#!/bin/bash' >> $LIVY_HOME/conf/livy-env.sh
+echo 'export SPARK_HOME=/opt/spark-2.4.6-bin-hadoop2.7' >> $LIVY_HOME/conf/livy-env.sh 
+
 # Create `livy.conf` file
 echo livy.spark.master = spark://$SPARK_MASTER_ENDPOINT:$SPARK_MASTER_PORT >> $LIVY_HOME/conf/livy.conf
 echo livy.file.local-dir-whitelist = $LIVY_FILE_LOCAL_DIR_WHITELIST >> $LIVY_HOME/conf/livy.conf
